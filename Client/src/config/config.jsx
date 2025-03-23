f6805535ab935c653e0fd1edf1906c910b4b753a
praveen choudhary <pc852180@gmail.com>
2025-03-24 00:04:36 +0530

    E-Banking-Project

--- Client/src/config/config.jsx
+++ Client/src/config/config.jsx
@@ -1,5 +1,5 @@
 
 
-const BASE_URL = "http://localhost:8000";
-
+const BASE_URL = "https://e-banking-system-q6yd.onrender.com"
+//https://e-banking-system-q6yd.onrender.com
 export default BASE_URL;
